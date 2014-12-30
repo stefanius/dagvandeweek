0c68b8292df328438c5f131ea2a09c25332c5fdf
stefanius <KIT_kat_16>
2014-12-30 23:36:04 +0100

    Added WeekHero

--- sites/dagvandeweek/public_html/src/Stef/DagVanDeWeekBundle/Controller/BaseController.php
+++ sites/dagvandeweek/public_html/src/Stef/DagVanDeWeekBundle/Controller/BaseController.php
@@ -6,6 +6,7 @@ use Ivory\GoogleMap\Map;
 use Stef\DagVanDeWeekBundle\Manager\CalendarYearManager;
 use Stef\DagVanDeWeekBundle\Manager\HistoryManager;
 use Stef\DagVanDeWeekBundle\Manager\HistoryYearManager;
+use Stef\DagVanDeWeekBundle\Manager\WeekHeroManager;
 use Stef\SimpleCmsBundle\KeyValueParser\Parser;
 use Stef\SimpleCmsBundle\Manager\DictionaryManager;
 use Stef\SimpleCmsBundle\Manager\NewsManager;
@@ -72,6 +73,14 @@ class BaseController extends Controller
     protected function getHistoryManager()
     {
         return $this->get('stef_simple_cms.history_manager');
+    }
+
+    /**
+     * @return WeekHeroManager
+     */
+    protected function getWeekHeroManager()
+    {
+        return $this->get('stef_simple_cms.weekhero_manager');
     }
 
     /**

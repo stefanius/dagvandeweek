c05d90a08ea2f49dce81a64e02f62c2a6ad3b067
stefanius <KIT_kat_16>
2015-04-12 22:15:19 +0200

    Added Contact formType and updated controller

--- sites/dagvandeweek/public_html/src/Stef/DagVanDeWeekBundle/Controller/IndexController.php
+++ sites/dagvandeweek/public_html/src/Stef/DagVanDeWeekBundle/Controller/IndexController.php
@@ -2,10 +2,12 @@
 
 namespace Stef\DagVanDeWeekBundle\Controller;
 
+use Stef\DagVanDeWeekBundle\Entity\Contact;
+use Stef\DagVanDeWeekBundle\Form\ContactType;
 use Stef\SimpleCmsBundle\Entity\Page;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
-class IndexController extends Controller
+class IndexController extends BaseController
 {
     public function indexAction()
     {
@@ -15,6 +17,41 @@ class IndexController extends Controller
 
         return $this->render('StefDagVanDeWeekBundle:Default:index.html.twig', [
             'page' => $page
+        ]);
+    }
+
+    public function contactAction(Request $request)
+    {
+        $form = $this->createForm(new ContactType());
+
+        if ($request->isMethod('POST')) {
+            $form->bind($request);
+
+            if ($form->isValid()) {
+                $contact = new Contact();
+                $contact->setEmail($form->get('email')->getData());
+                $contact->setName($form->get('name')->getData());
+                $contact->setReason($form->get('reason')->getData());
+                $contact->setPhone($form->get('phone')->getData());
+                $contact->setIp($request->getClientIp());
+                $contact->setSummary($form->get('summary')->getData());
+                $contact->setModified(new \DateTime());
+                $contact->setCreated(new \DateTime());
+
+                $manager = $this->getContactManager();
+                $manager->persistAndFlush($contact);
+
+                return $this->redirect($this->generateUrl('stef_dag_van_de_week_contact'));
+            }
+        }
+
+        $page = new Page();
+        $page->setDescription('Neem vrijblijvend contact op met de redactie achter DagVanDeWeek.nl!');
+        $page->setTitle('Contact');
+
+        return $this->render('StefDagVanDeWeekBundle:Default:contact.html.twig', [
+            'page' => $page,
+            'form' => $form->createView(),
         ]);
     }
 }

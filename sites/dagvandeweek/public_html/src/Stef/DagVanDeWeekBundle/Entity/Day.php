f4c31f5d7cee2fce34836b690fb6400affbdc48e
stefanius <KIT_kat_16>
2015-06-04 07:33:17 +0200

    Added DayByDay section.

---
+++ sites/dagvandeweek/public_html/src/Stef/DagVanDeWeekBundle/Entity/Day.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Stef\DagVanDeWeekBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Stef\SimpleCmsBundle\Entity\AbstractCmsContent;
+
+/**
+ * Day
+ *
+ * @ORM\Table(uniqueConstraints={@ORM\UniqueConstraint(name="unique_day_month", columns={"day", "month"})})
+ * @ORM\Entity
+ */
+class Day extends AbstractCmsContent
+{
+    /**
+     * @ORM\Column(type="string", nullable=true)
+     */
+    protected $title;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="day", type="integer", length=255)
+     */
+    protected $day;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="month", type="integer", length=255)
+     */
+    protected $month;
+
+    /**
+     * @return int
+     */
+    public function getDay()
+    {
+        return $this->day;
+    }
+
+    /**
+     * @param int $day
+     */
+    public function setDay($day)
+    {
+        $this->day = $day;
+    }
+
+    /**
+     * @return int
+     */
+    public function getMonth()
+    {
+        return $this->month;
+    }
+
+    /**
+     * @param int $month
+     */
+    public function setMonth($month)
+    {
+        $this->month = $month;
+    }
+}

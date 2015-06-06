ae935daba43e77a593c83e67c97e6ba883fc1e55
stefanius <KIT_kat_16>
2015-06-06 21:34:47 +0200

    Misc updates

--- sites/dagvandeweek/public_html/src/Stef/DagVanDeWeekBundle/Controller/TodayInThePastController.php
+++ sites/dagvandeweek/public_html/src/Stef/DagVanDeWeekBundle/Controller/TodayInThePastController.php
@@ -2,6 +2,7 @@
 
 namespace Stef\DagVanDeWeekBundle\Controller;
 
+use Stef\DagVanDeWeekBundle\Date\DateObjectFactory;
 use Stef\SimpleCmsBundle\Entity\Page;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -16,26 +17,21 @@ class TodayInThePastController extends BaseController
     public function showTodayAction(Request $request)
     {
         $today = new \DateTime();
-        $year  = $today->format('Y');
-        $month = $today->format('m');
-        $day   = $today->format('d');
+        $factory = new DateObjectFactory();
+        $date    = $factory->createByDateTime($today);
 
         $manager   = $this->getHistoryManager();
-        $histories = $manager->findByDayMonth($day, $month);
-
-        $dayinfo = $this->createDayInfo($year, $month, $day);
+        $histories = $manager->findByDayMonth($date->getDay(), $date->getMonthNumber());
 
         $page = new Page();
         $page->setTitle('Vandaag in het verleden');
-        $page->setDescription('De dag van vandaag heeft een verleden. Bekijk hier de datum van vandaag en duik in het verleden van ' . $day . ' ' . $dayinfo['dutchMonthName']);
+        $page->setDescription('De dag van vandaag heeft een verleden. Bekijk hier de datum van vandaag en duik in het verleden van ' . $date->getDay(). ' ' . $date->getTranslatedMonthName());
 
-        return $this->render('StefDagVanDeWeekBundle:TodayInThePast:index.html.twig', array_merge($dayinfo, [
+        return $this->render('StefDagVanDeWeekBundle:TodayInThePast:index.html.twig', [
                 'page'      => $page,
-                'year'      => $year,
-                'month'     => $month,
-                'day'       => $day,
                 'histories' => $histories,
-            ])
+                'date'      => $date,
+            ]
         );
     }
 }

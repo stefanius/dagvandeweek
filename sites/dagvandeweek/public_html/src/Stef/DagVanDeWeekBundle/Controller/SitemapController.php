8157f4b62a3818eeca75f7033f6a57797a0fb401
Stefanius <sgrootveld@gmail.com>
2016-05-01 19:10:05 +0200

    Update sitemaps.

--- sites/dagvandeweek/public_html/src/Stef/DagVanDeWeekBundle/Controller/SitemapController.php
+++ sites/dagvandeweek/public_html/src/Stef/DagVanDeWeekBundle/Controller/SitemapController.php
@@ -2,6 +2,7 @@
 
 namespace Stef\DagVanDeWeekBundle\Controller;
 
+use Stef\DagVanDeWeekBundle\CalendarTranslations\Dutch;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -31,5 +32,48 @@ class SitemapController extends BaseController
             ],
             $response
         );
+    }
+
+    public function generateByMonthAction(Request $request, $month)
+    {
+        $months = new Dutch();
+        $response = new Response();
+        $response->headers->set('Content-Type', 'application/xml');
+
+        $number = $months->getMonthNumberByName($month);
+
+        if ($number > 0) {
+            $records = $this->getHistoryManager()->findByMonth($number);
+
+            return $this->render('StefDagVanDeWeekBundle:Sitemap:history.xml.twig', [
+                'records' => $records,
+            ],
+                $response
+            );
+        }
+
+        return $this->redirect('/');
+    }
+
+    public function generateByMonthPerDayAction(Request $request, $month)
+    {
+        $months = new Dutch();
+        $response = new Response();
+        $response->headers->set('Content-Type', 'application/xml');
+
+        $number = $months->getMonthNumberByName($month);
+
+        if ($number > 0) {
+            $records = $this->getHistoryManager()->getActiveDays($number);
+
+            return $this->render('StefDagVanDeWeekBundle:Sitemap:activedays.xml.twig', [
+                'records' => $records,
+                'dutchMonthName' => $month,
+            ],
+                $response
+            );
+        }
+
+        return $this->redirect('/');
     }
 }

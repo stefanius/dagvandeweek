7785cf797fe0dd32b1111841d633c9065b683897
Stefanius <sgrootveld@gmail.com>
2016-05-20 21:31:56 +0200

    Install Dutch locales.

--- after.sh
+++ after.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
+sudo locale-gen nl_NL.utf8
+sudo update-locale
+
 php /home/vagrant/dagvandeweek/app/console doctrine:schema:create
 php /home/vagrant/dagvandeweek/app/console fos:user:create admin admin@stefanius.nl 1234

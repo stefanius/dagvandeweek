555f5a6f3bac1c54b46f6682e12005b90ae1c3fe
Stefanius <sgrootveld@gmail.com>
2016-05-24 22:35:35 +0200

    Update testsuite.

--- tests/FunctionalTests/ByUrl/Kalender/CalenderPageTest.php
+++ tests/FunctionalTests/ByUrl/Kalender/CalenderPageTest.php
@@ -19,6 +19,59 @@ class CalenderPageTest extends AbstractFunctionalTestCase
 
         $this->assertEquals('Kalender ' . $year, $crawler->filter('h1')->eq(0)->text());
         $this->assertEquals('Maandoverzicht', $crawler->filter('h2:contains(Maandoverzicht)')->eq(0)->text());
+        $this->checkMonthNames($crawler, $year);
+    }
+
+    /**
+     * @dataProvider leapYearProvider
+     *
+     * @param $year
+     */
+    public function testLeapYearCalenderPages($year)
+    {
+        $crawler = $this->browse(sprintf("%s/%s", $this->route, $year));
+
+        $this->assertEquals('Kalender ' . $year, $crawler->filter('h1')->eq(0)->text());
+        $this->assertEquals('Maandoverzicht', $crawler->filter('h2:contains(Maandoverzicht)')->eq(0)->text());
+        $this->assertEquals('Schrikkeljaar', $crawler->filter('h2:contains(Schrikkeljaar)')->eq(0)->text());
+        $this->checkMonthNames($crawler, $year);
+    }
+
+    /**
+     * @dataProvider nonLeapYearProvider
+     *
+     * @param $year
+     */
+    public function testNonLeapYearCalenderPages($year)
+    {
+        $crawler = $this->browse(sprintf("%s/%s", $this->route, $year));
+
+        $this->assertEquals('Kalender ' . $year, $crawler->filter('h1')->eq(0)->text());
+        $this->assertEquals('Maandoverzicht', $crawler->filter('h2:contains(Maandoverzicht)')->eq(0)->text());
+        $this->assertEquals('Geen Schrikkeljaar', $crawler->filter('h2:contains(Geen Schrikkeljaar)')->eq(0)->text());
+        $this->checkMonthNames($crawler, $year);
+    }
+
+    protected function checkMonthNames($crawler, $year)
+    {
+        $months = [
+            'Januari',
+            'Februari',
+            'Maart',
+            'April',
+            'Mei',
+            'Juni',
+            'Juli',
+            'Augustus',
+            'September',
+            'Oktober',
+            'November',
+            'December',
+        ];
+
+        foreach ($months as $month) {
+            $this->assertEquals($month . ' ' . $year, $crawler->filter('h3:contains(' . $month . ')')->eq(0)->text());
+        }
     }
 
     /**
@@ -35,5 +88,44 @@ class CalenderPageTest extends AbstractFunctionalTestCase
         }
 
         return $array;
+    }
+
+    /**
+     * @return array
+     */
+    public function leapYearProvider()
+    {
+        return [
+            [1960],
+            [1980],
+            [1996],
+            [2012],
+            [1940],
+            [1964],
+            [1972],
+            [1976],
+            [2000],
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function nonLeapYearProvider()
+    {
+        return [
+            [1961],
+            [1981],
+            [1997],
+            [2013],
+            [1941],
+            [1942],
+            [1965],
+            [1700],
+            [1800],
+            [1900],
+            [2100],
+            [2200],
+        ];
     }
 }

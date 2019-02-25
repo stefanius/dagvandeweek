5d17fc4e3910373a7cfdc34822dbbf64237578c2
sgrootveld <sgrootveld@gmail.com>
2019-02-25 20:12:47 +0100

    Add website controllers for Historic Persons

--- resources/views/pages/website/persons/show.blade.php
+++ resources/views/pages/website/persons/show.blade.php
@@ -3,12 +3,13 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
+
         <div class="col-md-8">
             <h1>{{ $person->first_name }} {{ $person->last_name }}</h1>
 
             {{ $person->body }}
-
         </div>
+
     </div>
 </div>
 @endsection
